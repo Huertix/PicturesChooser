8cfa57e5e9236ad4982d1277b53b1c2ee6c96de2
Huertix <dhuerta23@gmail.com>
2022-02-17 10:51:56 +0100

    clean

--- MoveFiles.ps1
+++ MoveFiles.ps1
@@ -1,17 +1,13 @@
-$_SourcePath = "C:\Users\erikk\Desktop\test1"
-$_DestinationPath = "C:\Users\erikk\Desktop\test2"
-$_list = "C:\Users\erikk\Desktop\lista.txt"
+$_SourcePath = "C:\Users\tom\Desktop\test1"
+$_DestinationPath = "C:\Users\tom\Desktop\test2"
+$_list = "C:\Users\tom\Desktop\lista.txt"
 
 foreach($line in Get-Content $_list) {
-    if($line -match $regex){
-        # Work here
-    }
-
-    Write-Output -InputObject  (Test-Path -Path $_SourcePath+"\"+$line  -PathType Leaf)
-    Write-Output -InputObject $_SourcePath\$line
+    #Write-Output -InputObject  (Test-Path -Path $_SourcePath+"\"+$line  -PathType Leaf)
+    #Write-Output -InputObject $_SourcePath\$line
     if (Test-Path -Path $_SourcePath\$line  -PathType Leaf) {
         Write-Output -InputObject "Moving....."
-        Write-Output -InputObject $_SourcePath+$line
+        Write-Output -InputObject $_SourcePath\$line
     	Move-item –path $_SourcePath\$line –destination $_DestinationPath\$line
     }
 }
